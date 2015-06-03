bcfac8a40160da538d6fe879ab634b76b6553503
Nate Goldman <nate@ngoldman.me>
2015-06-03 14:19:22 -0700

    :factory: fix tests

--- test/model-test.js
+++ test/model-test.js
@@ -1,6 +1,6 @@
 var should = require('should'),
   config = require('config'),
-  koop = require('koop-server/lib');
+  koop = require('koop/lib');
 
 var repo = 'geodata',
   user = 'chelm',
@@ -8,7 +8,7 @@ var repo = 'geodata',
   key = [repo, user, file].join('/');
 
 before(function (done) {
-  koop.Cache.db = koop.PostGIS.connect( config.db.postgis.conn );
+  koop.Cache.db = koop.LocalDB;
   var data_dir = __dirname + '/output/';
   koop.Cache.data_dir = data_dir;
   Gist = new require('../models/Gist.js')( koop );
@@ -17,31 +17,26 @@ before(function (done) {
 
 describe('Github Model', function(){
 
-    describe('when caching a github file', function(){
-       before(function(done ){
-        // connect the cache
-        //Cache.db = PostGIS.connect( config.db.postgis.conn );
+  describe('when caching a github file', function(){
+
+    it('should find the repo and return the data', function(done){
+      Gist.find(6178185, {}, function(err, data){
+        should.not.exist(err);
+        should.exist(data);
+        data.length.should.equal(2);
         done();
       });
-
-      it('should find the repo and return the data', function(done){
-        Gist.find(6178185, {}, function(err, data){
-          should.not.exist(err);
-          should.exist(data);
-          data.length.should.equal(1);
-          done();
-        });
-      });
-
-      it('should find the repo and return the data', function(done){
-        Gist.checkCache(6178185, [{updated_at:1234}], {}, function(err, data){
-          should.not.exist(err);
-          should.exist(data);
-          data.length.should.equal(2);
-          done();
-        });
-      }); 
-    
     });
-});
 
+    it('should find the repo and return the data', function(done){
+      Gist.checkCache(6178185, [{updated_at:1234}], {}, function(err, data){
+        should.not.exist(err);
+        should.exist(data);
+        data.length.should.equal(2);
+        done();
+      });
+    });
+
+  });
+
+});
--- test/routes-test.js
+++ test/routes-test.js
@@ -1,73 +1,61 @@
 var should = require('should'),
   request = require('supertest'),
   config = require('config'),
-  koop = require('koop-server')(config),
-  kooplib = require('koop-server/lib');
+  koop = require('koop')(config),
+  kooplib = require('koop/lib');
 
 before(function(done){
   var provider = require('../index.js');
-  model = new provider.model( kooplib );
-  controller = new provider.controller( model );
+  var model = new provider.model( kooplib );
+  var controller = new provider.controller( model, kooplib.BaseController );
   koop._bindRoutes( provider.routes, controller );
   done();
 });
 
 describe('Koop Routes', function(){
 
-   /* describe('/gist', function() {
-      it('should return 200', function(done) {
-        request(koop)
-          .get('/gist')
-          .end(function(err, res){
-            res.should.have.status(200);
-            done();
-        });
-      });
-    });*/
-
-    describe('/gist/6021269', function() {
-      it('should return 200', function(done) {
-        request(koop)
-          .get('/gist/6021269')
-          .end(function(err, res){
-            res.should.have.status(200);
-            done();
-        });
+  describe('/gist/6021269', function() {
+    it('should return 200', function(done) {
+      request(koop)
+        .get('/gist/6021269')
+        .end(function(err, res){
+          res.should.have.status(200);
+          done();
       });
     });
+  });
 
-    describe('/gist/6021269/FeatureServer', function() {
-      it('should return 200', function(done) {
-        request(koop)
-          .get('/gist/6021269/FeatureServer')
-          .end(function(err, res){
-            res.should.have.status(200);
-            done();
-        });
+  describe('/gist/6021269/FeatureServer', function() {
+    it('should return 200', function(done) {
+      request(koop)
+        .get('/gist/6021269/FeatureServer')
+        .end(function(err, res){
+          res.should.have.status(200);
+          done();
       });
     });
+  });
 
-    describe('/gist/6021269/FeatureServer/0', function() {
-      it('should return 200', function(done) {
-        request(koop)
-          .get('/gist/6021269/FeatureServer/0')
-          .end(function(err, res){
-            res.should.have.status(200);
-            done();
-        });
+  describe('/gist/6021269/FeatureServer/0', function() {
+    it('should return 200', function(done) {
+      request(koop)
+        .get('/gist/6021269/FeatureServer/0')
+        .end(function(err, res){
+          res.should.have.status(200);
+          done();
       });
     });
+  });
 
-    describe('/gist/6021269/FeatureServer/0/query', function() {
-      it('should return 200', function(done) {
-        request(koop)
-          .get('/gist/6021269/FeatureServer/0/query')
-          .end(function(err, res){
-            res.should.have.status(200);
-            done();
-        });
+  describe('/gist/6021269/FeatureServer/0/query', function() {
+    it('should return 200', function(done) {
+      request(koop)
+        .get('/gist/6021269/FeatureServer/0/query')
+        .end(function(err, res){
+          res.should.have.status(200);
+          done();
       });
     });
+  });
 
 });
-
