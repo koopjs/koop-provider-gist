cddeb45dff7e8f58e01a44a7c5dbd55d048e0884
Nate Goldman <nate@ngoldman.me>
2015-08-25 14:31:11 -0700

    :horse: improve example, point to local koop-gist

--- example/index.js
+++ example/index.js
@@ -1,12 +1,26 @@
-var koop = require('koop')({})
-var koopGist = require('koop-gist')
-
-koop.register(koopGist)
-
 var express = require('express')
+var logger = require('morgan')
+var errorHandler = require('errorhandler')
+var koop = require('koop')({
+  ghtoken: process.env.KOOP_GHTOKEN || null
+})
+var gist = require('../')
 var app = express()
 
+koop.register(gist)
+
+app.set('port', process.env.PORT || 3000)
 app.use(koop)
+
+if (app.get('env') === 'development') {
+  app.use(logger('dev'))
+  app.use(errorHandler({ dumpExceptions: true, showStack: true }))
+}
+
+if (app.get('env') === 'production') {
+  app.use(logger())
+  app.use(errorHandler())
+}
 
 app.get('/', function (req, res) {
   res.redirect('/gist')
